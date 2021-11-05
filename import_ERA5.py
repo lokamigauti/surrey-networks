bd98e3c9ca37ecefb32d22dc83b1da2f622eb9ed
Leonardo Kamigauti <leonardo.kamigauti@usp.br>
2021-11-05 13:27:21 +0000

    Correct DATA_DIR

--- import_ERA5.py
+++ import_ERA5.py
@@ -1,19 +1,52 @@
 #!/usr/bin/env python
-
+import numpy as np
 import cdsapi
+DATA_DIR = 'G:/My Drive/IC/Doutorado/Sandwich/Data/ERA5/'
 
-c = cdsapi.Client()
+def retrieve_wind(c, year, months, days, hours, area = [0, 0, 0, 0]):
+    if area == [0, 0, 0, 0]:
+        c.retrieve(
+            'reanalysis-era5-single-levels',
+            {
+                'product_type': 'reanalysis',
+                'param': wind,
+                'year': year,
+                'month': months,
+                'day': days,
+                'time': hours,
+                'format': 'netcdf',
+            },
+            DATA_DIR + f'wind_{year}.nc')
+    c.retrieve(
+        'reanalysis-era5-single-levels',
+        {
+            'product_type': 'reanalysis',
+            'variable': wind,
+            'year': year,
+            'month': months,
+            'day': days,
+            'time': hours,
+            'format': 'netcdf',
+            'area': area
+        },
+        DATA_DIR + f'wind_{year}.nc')
 
-c.retrieve(
-    'reanalysis-era5-pressure-levels',
-    {
-        'product_type': 'reanalysis',
-        'variable': 'temperature',
-        'pressure_level': '1000',
-        'year': '2008',
-        'month': '01',
-        'day': '01',
-        'time': '12:00',
-        'format': 'netcdf',  # Supported format: grib and netcdf. Default: grib
-    },
-    'download.nc')  # Output file. Adapt as you wish.
+
+
+if __name__ == '__main__':
+    c = cdsapi.Client()
+
+    wind = [
+        '10m_u_component_of_wind', '10m_v_component_of_wind',
+    ]
+
+    UK_data_window = [
+        61, -21, 46, 12
+    ]
+    hours = [f'{x:02d}' + ':00' for x in np.arange(0, 24)]
+    months = [f'{x:02d}' for x in np.arange(1, 13)][:2]
+    days = [f'{x:02d}' for x in np.arange(1, 32)][:2]
+    years = [2021]
+    for year in years:
+        retrieve_wind(c, year, months, days, hours, UK_data_window)
+
