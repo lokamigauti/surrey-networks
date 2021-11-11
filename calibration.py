cb4388dceb230ae7abba5a1e6f5cb1b01dd78226
Leonardo Kamigauti <leonardo.kamigauti@usp.br>
2021-11-11 21:37:27 +0000

    Found hyperparameters

--- calibration.py
+++ calibration.py
@@ -9,45 +9,60 @@ DATA_DIR = 'G:/My Drive/IC/Doutorado/Sandwich/Data/'
 LCS = 'WokingNetwork/'
 
 
-def apply_ridge(X_train, y_train, X_test, y_test, alpha):
-    scaler_train = StandardScaler().fit(X_train)
-    X_train_scaled = scaler_train.transform(X_train)
+def test_ridge(X_train, y_train, X_test, y_test, alpha, plot=False):
+    scaler = StandardScaler().fit(X_train)
+    X_train_scaled = scaler.transform(X_train)
     reg = linear_model.Ridge(alpha=alpha)
     reg.fit(X_train_scaled, y_train)
 
-    scaler_test = StandardScaler().fit(X_test)
-    X_test_scaled = scaler_test.transform(X_test)
-    pred = reg.predict(X_test_scaled)
+    X_test_scaled = scaler.transform(X_test)
+    y_pred = reg.predict(X_test_scaled)
 
-    fig, ax = plt.subplots()
-    ax.plot(x=y_test, y=x_test, '.')
-    ax.plot(x=y_test, y=pred, '.')
-    x_vals = np.array(axes.get_xlim())
-    y_vals = x_vals
-    plt.plot(x_vals, y_vals, '--')
-    plt.title(f'{alpha}')
-    plt.show()
+    coef = reg.coef_
+    intercept = reg.intercept_
 
-    # df[['S1_ridge', 'S1_PM10', 'Ref_PM10']].plot(x='Ref_PM10', style='.')
-    # axes = plt.gca()
-    # x_vals = np.array(axes.get_xlim())
-    # y_vals = x_vals
-    # plt.plot(x_vals, y_vals, '--')
-    # plt.title(f'{alpha}')
-    # plt.show()
+    if explore:
+        fig, ax = plt.subplots()
+        ax.plot(y_train, X_train[:,1], '.', label='sensor')
+        ax.plot(y_train, reg.predict(X_train_scaled), '.', label='calibration')
+        x_vals = np.array(ax.get_xlim())
+        y_vals = x_vals
+        ax.plot(x_vals, y_vals, '--')
+        ax.set_title(f'train{alpha}')
+        ax.legend(loc='upper left')
+        plt.show()
+
+        fig, ax = plt.subplots()
+        ax.plot(y_test, X_test[:,1], '.', label='sensor')
+        ax.plot(y_test, y_pred, '.', label='calibration')
+        x_vals = np.array(ax.get_xlim())
+        y_vals = x_vals
+        ax.plot(x_vals, y_vals, '--')
+        ax.set_title(f'{alpha}')
+        ax.legend(loc='upper left')
+        plt.show()
+
+    return coef, intercept
 
 
-def apply_ridge_poly(X_train, y_train, X_test, y_test, alpha, degree):
-    poly_features = PolynomialFeatures(degree=degree, include_bias=False)
+def test_ridge_poly(X_train, y_train, X_test, y_test, alpha, degree, plot=False):
+    poly_features = PolynomialFeatures(degree=degree)
     X_train_poly = poly_features.fit_transform(X_train)
     X_test_poly = poly_features.fit_transform(X_test)
-    apply_ridge(X_train_poly, y_train, X_test_poly, y_test, alpha)
+    coef, intercept = test_ridge(X_train_poly, y_train, X_test_poly, y_test, alpha, plot=True)
+
+    return coef, intercept
 
 
 if __name__ == '__main__':
     df = pd.read_csv(DATA_DIR+LCS+'calibration.csv', parse_dates=['time'], index_col='time')
-    y = df['Ref_PM10']
-    X = df[['S1_Temp', 'S1_RH', 'S1_PM10']]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .2, random_state = 42)
+    y = df['Ref_PM10'].to_numpy()
+    X = df[['S1_PM10', 'S1_Temp', 'S1_RH']].to_numpy()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.20, random_state=18462)
 
-    apply_ridge_poly(X_train, y_train, X_test, y_test, .001, 10)
+    coef, intercept = test_ridge_poly(X_train, y_train, X_test, y_test, 30, 3, plot=True) # manual test of hyperparameters
+    coef
+    intercept
+
+
+
