f7cd73947b6ec38b3d8f2f871b8c328bfe070fb4
Leonardo Kamigauti <leonardo.kamigauti@usp.br>
2021-11-14 17:26:02 +0000

    Apply regression all stations

--- calibration.py
+++ calibration.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import xarray as xr
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 from sklearn.preprocessing import PolynomialFeatures
@@ -23,7 +24,7 @@ def test_ridge(X_train, y_train, X_test, y_test, alpha, plot=False):
 
     if plot:
         fig, ax = plt.subplots()
-        ax.plot(y_train, X_train[:,1], '.', label='sensor')
+        ax.plot(y_train, X_train[:,0], '.', label='sensor')
         ax.plot(y_train, reg.predict(X_train_scaled), '.', label='calibration')
         x_vals = np.array(ax.get_xlim())
         y_vals = x_vals
@@ -33,7 +34,7 @@ def test_ridge(X_train, y_train, X_test, y_test, alpha, plot=False):
         plt.show()
 
         fig, ax = plt.subplots()
-        ax.plot(y_test, X_test[:,1], '.', label='sensor')
+        ax.plot(y_test, X_test[:,0], '.', label='sensor')
         ax.plot(y_test, y_pred, '.', label='calibration')
         x_vals = np.array(ax.get_xlim())
         y_vals = x_vals
@@ -46,7 +47,7 @@ def test_ridge(X_train, y_train, X_test, y_test, alpha, plot=False):
 
 
 def test_ridge_poly(X_train, y_train, X_test, y_test, alpha, degree, plot=False):
-    poly_features = PolynomialFeatures(degree=degree)
+    poly_features = PolynomialFeatures(degree=degree, include_bias=False)
     X_train_poly = poly_features.fit_transform(X_train)
     X_test_poly = poly_features.fit_transform(X_test)
     coef, intercept = test_ridge(X_train_poly, y_train, X_test_poly, y_test, alpha, plot=True)
@@ -69,15 +70,85 @@ def calibration_data_import(path):
     da = da.transpose('time', 'station', 'variable')
     return da
 
+def apply_poly_ridge(X, y, degree, alpha):
+    poly_features = PolynomialFeatures(degree=degree, include_bias=False)
+    X_poly = poly_features.fit_transform(X)
+    scaler = StandardScaler().fit(X_poly)
+    X_poly_scaled = scaler.transform(X_poly)
+
+    reg = linear_model.Ridge(alpha=alpha)
+    reg.fit(X_poly_scaled, y)
+
+    return_dict = {'coef': reg.coef_,
+                   'intercept': reg.intercept_,
+                   'mean': scaler.mean_,
+                   'std': np.sqrt(scaler.var_),
+                   'poly_degree': degree}
+
+    return return_dict
+
+def ds_regression(X, y, model_dims=['station'], **regression_kwargs):
+    X = X.copy()
+    y = y.copy()
+
+    if len(model_dims) > 1:
+        X = X.stack(model_dim=model_dims)
+    else:
+        X = X.rename({model_dims[0]: 'model_dim'})
+
+    params_dict = {}
+    for dict_key in X.model_dim.values:
+        params_dict[dict_key] = apply_poly_ridge(X.sel(model_dim=dict_key), y, **regression_kwargs)
+
+    return params_dict
+
+
+def calibrate(station_output, params):
+    poly_features = PolynomialFeatures(degree=params['poly_degree'], include_bias=False)
+    station_output_poly = poly_features.fit_transform(station_output)
+    station_output_poly_normalised = (station_output_poly - params['mean'])/params['std']
+    polys = station_output_poly_normalised * params['coef']
+    y = params['intercept'] + polys.sum(axis=1)
+    return y
+
+
 if __name__ == '__main__':
     path = DATA_DIR + LCS + 'calibration.csv'
     da = calibration_data_import(path)
-    y = da.sel(station='Ref', variable='PM10').values.copy()
-    X = da.sel(station='S1', variable=['PM10', 'Temp', 'RH']).values.copy()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.20, random_state=18462)
-    alpha = 30
-    degree = 3
 
     # The hyperparameters determination was performed by eye because of the low number of samples
+    # y = da.sel(station='Ref', variable='PM10').values.copy()
+    # X = da.sel(station='S1', variable=['PM10', 'RH', 'Temp']).values.copy()
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.20, random_state=18462)
+    # alpha = 30
+    # degree = 3
+    # coef, intercept = test_ridge_poly(X_train, y_train, X_test, y_test, alpha, degree, plot=True)
+    # coef, intercept = test_ridge_poly(X, y, X, y, alpha, degree, plot=True)
 
-    coef, intercept = test_ridge_poly(X_train, y_train, X_test, y_test, alpha, degree, plot=True)
+    alpha = 30
+    degree = 3
+    targets = ['PM10', 'PM2.5', 'PM1']
+    regression_params = {}
+    for target in targets:
+        y = da.sel(station='Ref', variable=target)
+        X = da.drop_sel(station='Ref').sel(variable=[target, 'RH', 'Temp'])
+        regression_params[target] = ds_regression(X, y, degree=degree, alpha=alpha)
+
+
+    X = da.sel(station='S5', variable=['PM1', 'RH', 'Temp']).values.copy()
+    X_cal = calibrate(X, regression_params['PM1']['S5']).copy()
+
+
+    fig, ax = plt.subplots()
+    ax.plot(y, X[:, 0], '.', label='sensor')
+    ax.plot(y, X_cal, '.', label='calibration')
+    x_vals = np.array(ax.get_xlim())
+    y_vals = x_vals
+    ax.plot(x_vals, y_vals, '--')
+    ax.set_title(f'train{alpha}')
+    ax.legend(loc='upper left')
+    plt.show()
+
+
+
+    da.sel(station='S1')['PM10_cal'] = calibrate(X, regression_params['PM10']['S1']).copy()
