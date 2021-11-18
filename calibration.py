2f3588acec6e0bd41ef11bfcab971144b32b54a6
Leonardo Kamigauti <leonardo.kamigauti@usp.br>
2021-11-18 13:16:47 +0000

    Make calibration file and clean main

--- calibration.py
+++ calibration.py
@@ -94,7 +94,7 @@ def apply_poly_ridge(X, y, degree, alpha):
 
     return return_dict
 
-def ds_regression(X, y, model_dims=['station'], **regression_kwargs):
+def apply_model_on_dimensions(X, y, model_dims=['station'], **regression_kwargs):
     X = X.copy()
     y = y.copy()
 
@@ -108,6 +108,23 @@ def ds_regression(X, y, model_dims=['station'], **regression_kwargs):
         params_dict[dict_key] = apply_poly_ridge(X.sel(model_dim=dict_key), y, **regression_kwargs)
 
     return params_dict
+
+def get_ridge_parameters(alpha, degree, targets, save=False):
+    regression_params = {}
+    for target in targets:
+        y = da.sel(station='Ref', variable=target)
+        X = da.drop_sel(station='Ref').sel(variable=[target, 'rh', 'temp'])
+        regression_params[target] = apply_model_on_dimensions(X, y, degree=degree, alpha=alpha)
+
+    if save:
+        with open(OUTPUT_DIR + LCS + 'calibration_parameters.json', 'w') as fp:
+            json.dump(regression_params, fp, default=dict2json)
+
+    # Import example:
+    # with open(OUTPUT_DIR + LCS + 'calibration_parameters.json', 'r') as fp:
+    #     calibration_params = json.load(fp, object_hook=deconvert)
+
+    return regression_params
 
 
 def calibrate(station_outputs, params):
@@ -132,53 +149,86 @@ def calibrate(station_outputs, params):
             y[n] = params['intercept'] + polys.sum()
     return y
 
-def convert(x):
+def dict2json(x):
     if hasattr(x, "tolist"):  # numpy arrays have this
         return {"$array": x.tolist()}  # Make a tagged object
     raise TypeError(x)
 
 
-def deconvert(x):
+def json2dict(x):
     if len(x) == 1:  # Might be a tagged object...
         key, value = next(iter(x.items()))  # Grab the tag and value
         if key == "$array":  # If the tag is correct,
             return np.array(value)  # cast back to array
     return x
 
-def open_calibration_data(path):
+
+def calibrator(data, target, calibration_params):
+    X = data.sel(variable=[target, 'rh', 'temp']).values.copy()
+    station_ = data.station.values[0]
+    y = calibration.calibrate(X.reshape(X.shape[1:3]).transpose(), calibration_params[target][station_]).copy()
+    da = xr.DataArray(
+        y.reshape(-1, 1),
+        coords=[('time', data.time.values.copy()), ('variable', [target+'_cal'])])
+    da = da.astype('float32')
+    return da
+
+
+def make_calibration(data, calibration_params, save=False):
+    da_calibrated = data.copy().rename('calibrated')
+    pms = ['pm10', 'pm25', 'pm1']
+    cal_list = []
+    for pm in pms:
+        cal = da_calibrated.groupby('station').map(calibrator, args=(pm, calibration_params)).copy().rename('case')
+        cal_list.append(cal)
+    da_calibrated = xr.concat(cal_list, dim='variable')
+    if save:
+        da_calibrated.to_netcdf(OUTPUT_DIR + LCS + 'pm_calibrated.nc')
+    return da_calibrated
+
+
+def import_json_as_dict(path):
     with open(path, 'r') as fp:
-        calibration_params = json.load(fp, object_hook=deconvert)
-    return calibration_params
+        dt = json.load(fp, object_hook=json2dict)
+    return dt
+
 
 if __name__ == '__main__':
-    path = DATA_DIR + LCS + 'calibration_std.csv'
-    da = calibration_data_import(path)
+    find_hyperparameters = False
+    calibrate_stations = False
 
-    # The hyperparameters determination was performed by eye because of the low number of samples
-    # y = da.sel(station='Ref', variable='PM10').values.copy()
-    # X = da.sel(station='S1', variable=['PM10', 'RH', 'temp']).values.copy()
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.20, random_state=18462)
-    # alpha = 30
-    # degree = 3
-    # coef, intercept = test_ridge_poly(X_train, y_train, X_test, y_test, alpha, degree, plot=True)
-    # coef, intercept = test_ridge_poly(X, y, X, y, alpha, degree, plot=True)
+    # load data
+    calibration_data_path = DATA_DIR + LCS + 'calibration_std.csv'
+    da = calibration_data_import(calibration_data_path)
+    station_data_path = DATA_DIR + 'Imported/lcs.nc'
+    data = xr.open_dataarray(station_data_path)
 
-    alpha = 30
-    degree = 3
-    targets = ['pm10', 'pm25', 'pm1']
-    regression_params = {}
-    for target in targets:
-        y = da.sel(station='Ref', variable=target)
-        X = da.drop_sel(station='Ref').sel(variable=[target, 'rh', 'temp'])
-        regression_params[target] = ds_regression(X, y, degree=degree, alpha=alpha)
+    if not calibrate_stations:
+        ridge_parameters_path = OUTPUT_DIR + LCS + 'calibration_parameters.json'
+        calibration_params = import_json_as_dict(ridge_parameters_path)
+        pm_calibrated_path = OUTPUT_DIR + LCS + 'pm_calibrated.nc'
+        pm_calibrated = xr.open_dataarray(pm_calibrated_path)
 
-    # regression_params_list = {a: b: c.tolist() for a, b, c in regression_params.items()}
-    with open(OUTPUT_DIR + LCS + 'calibration_parameters.json', 'w') as fp:
-        json.dump(regression_params, fp, default=convert)
-    with open(OUTPUT_DIR + LCS + 'calibration_parameters.json', 'r') as fp:
-        calibration_params = json.load(fp, object_hook=deconvert)
+    if find_hyperparameters:
+        # The hyperparameters determination was performed by eye because of the low number of samples
+        y = da.sel(station='Ref', variable='PM10').values.copy()
+        X = da.sel(station='S1', variable=['PM10', 'RH', 'temp']).values.copy()
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.20, random_state=18462)
+        alpha = 30
+        degree = 3
+        coef, intercept = test_ridge_poly(X_train, y_train, X_test, y_test, alpha, degree, plot=True)
+        coef, intercept = test_ridge_poly(X, y, X, y, alpha, degree, plot=True)
+
+    if calibrate_stations:
+        # make parameters json
+        alpha = 30
+        degree = 3
+        targets = ['pm10', 'pm25', 'pm1']
+        calibration_params = get_ridge_parameters(alpha, degree, targets, save=True)
+        pm_calibrated = make_calibration(data, calibration_params, save=True)
 
 
+    # Plots
     X = da.sel(station='WokingGreens#5', variable=['pm1', 'rh', 'temp']).values.copy()
     X_cal = calibrate(X, regression_params['pm1']['WokingGreens#5']).copy()
 
