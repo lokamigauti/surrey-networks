38f5527c677c808fd79bd7d8461215deae6de78f
Leonardo Kamigauti <leonardo.kamigauti@usp.br>
2021-11-25 19:09:03 +0000

    plot wind

--- analysis.py
+++ analysis.py
@@ -46,18 +46,18 @@ if __name__ == '__main__':
     da = xr.open_dataarray(DATA_DIR + 'Imported/lcs.nc')
     calibration_params = calibration.import_json_as_dict(OUTPUT_DIR + LCS + 'calibration_parameters.json')
 
-    #
-    # da_calibrated = da.copy().rename('calibrated')
-    # for pm in ['pm10', 'pm25', 'pm1']:
-    #     cal = da_calibrated.groupby('station').map(calibrator, args=(pm, calibration_params)).copy().rename('case')
-    #     da_calibrated = xr.concat([da_calibrated, cal], dim='variable')
-    # da_calibrated = make_calibration(da, calibration_params, save=True)
     da_calibrated = xr.open_dataarray(OUTPUT_DIR + LCS + 'pm_calibrated.nc')
     mean_pm_per_station = da_calibrated.stack(station_pm=('station', 'variable')).groupby('station_pm').mean('time')
     mean_pm = mean_pm_per_station.unstack().mean('station')
     mean_pm.sel(variable='pm10_cal')
     mean_pm.sel(variable='pm25_cal')
     mean_pm.sel(variable='pm1_cal')
+
+    std_pm_per_station = da_calibrated.stack(station_pm=('station', 'variable')).groupby('station_pm').std('time')
+    std_pm = median_pm_per_station.unstack().mean('station')
+    std_pm.sel(variable='pm10_cal')
+    std_pm.sel(variable='pm25_cal')
+    std_pm.sel(variable='pm1_cal')
 
     da_pm = da.sel(variable=['pm10', 'pm25', 'pm1'])
     mean_pm_per_station_uncal = da_pm.stack(station_pm=('station', 'variable')).groupby('station_pm').mean('time')
@@ -66,8 +66,21 @@ if __name__ == '__main__':
     mean_pm_uncal.sel(variable='pm25')
     mean_pm_uncal.sel(variable='pm1')
 
-    median_pm_per_station = da_calibrated.stack(station_pm=('station', 'variable')).groupby('station_pm').median('time')
-    median_pm = median_pm_per_station.unstack().median('station')
-    median_pm.sel(variable='pm10_cal')
-    median_pm.sel(variable='pm25_cal')
-    median_pm.sel(variable='pm1_cal')
+    era5 = xr.open_dataset(DATA_DIR+'ERA5/era5_2021.nc').isel(expver=0)
+    # era5 = era5.resample(time='1d').mean()
+    era5 = era5.assign_coords(longitude = (era5.coords['longitude'].values+180)%360-180)
+    era5 = era5.sortby('longitude')
+    da_toplot = era5.sel(time='2021-08-03T23:00:00') \
+         .coarsen(latitude=3, longitude=3, boundary='trim').mean()\
+         .sortby('latitude')
+
+    ax = plt.subplot(projection=ccrs.Orthographic(-0.560294, 51.315831))
+    p = da_toplot.plot.quiver(
+        x='longitude', y='latitude',
+        u='u10', v='v10',
+        subplot_kws=dict(projection=ccrs.Orthographic(-0.560294, 51.315831),
+                         facecolor="gray"),
+        transform=ccrs.PlateCarree(),
+    )
+    p.axes.coastlines()
+    plt.show()
