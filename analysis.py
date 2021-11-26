a80c276e044e4b3575efa7fd1b53b589ea420d5c
Leonardo Kamigauti <leonardo.kamigauti@usp.br>
2021-11-26 00:59:23 +0000

    plot wind in n max pm days

--- analysis.py
+++ analysis.py
@@ -4,7 +4,6 @@ import cartopy.crs as ccrs
 import calibration
 import numpy as np
 
-
 DATA_DIR = 'G:/My Drive/IC/Doutorado/Sandwich/Data/'
 OUTPUT_DIR = 'G:/My Drive/IC/Doutorado/Sandwich/Output/'
 LCS = 'WokingNetwork/'
@@ -40,47 +39,80 @@ LCS = 'WokingNetwork/'
 # ax.coastlines()
 # plt.show()
 
+def plot_era5_wind(data, sel_time, sel_title):
+    da_toplot = data.sel(time=sel_time)\
+        .sortby('longitude').sel(longitude=slice(-2.4, 2)).sortby('latitude').sel(latitude=slice(50, 52))
+
+    ax = plt.subplot(projection=ccrs.Orthographic(-0.554106, 51.318604))
+    p = da_toplot.plot.quiver(
+        x='longitude', y='latitude',
+        u='u10', v='v10',
+        subplot_kws=dict(projection=ccrs.Orthographic(-0.554106, 51.318604),
+                         facecolor="gray"),
+        transform=ccrs.PlateCarree(),
+    )
+    p.axes.coastlines()
+    plt.title(sel_title)
+    plt.show()
+
 
 if __name__ == '__main__':
-
-    da = xr.open_dataarray(DATA_DIR + 'Imported/lcs.nc')
+    other_vars = xr.open_dataarray(DATA_DIR + 'Imported/lcs.nc')
     calibration_params = calibration.import_json_as_dict(OUTPUT_DIR + LCS + 'calibration_parameters.json')
+    pm = xr.open_dataarray(OUTPUT_DIR + LCS + 'pm_calibrated.nc')
 
-    da_calibrated = xr.open_dataarray(OUTPUT_DIR + LCS + 'pm_calibrated.nc')
-    mean_pm_per_station = da_calibrated.stack(station_pm=('station', 'variable')).groupby('station_pm').mean('time')
+    mean_pm_per_station = pm.stack(station_pm=('station', 'variable')).groupby('station_pm').mean('time')
     mean_pm = mean_pm_per_station.unstack().mean('station')
     mean_pm.sel(variable='pm10_cal')
     mean_pm.sel(variable='pm25_cal')
     mean_pm.sel(variable='pm1_cal')
 
-    std_pm_per_station = da_calibrated.stack(station_pm=('station', 'variable')).groupby('station_pm').std('time')
+    std_pm_per_station = pm.stack(station_pm=('station', 'variable')).groupby('station_pm').std('time')
     std_pm = median_pm_per_station.unstack().mean('station')
     std_pm.sel(variable='pm10_cal')
     std_pm.sel(variable='pm25_cal')
     std_pm.sel(variable='pm1_cal')
 
-    da_pm = da.sel(variable=['pm10', 'pm25', 'pm1'])
-    mean_pm_per_station_uncal = da_pm.stack(station_pm=('station', 'variable')).groupby('station_pm').mean('time')
-    mean_pm_uncal = mean_pm_per_station_uncal.unstack().mean('station')
-    mean_pm_uncal.sel(variable='pm10')
-    mean_pm_uncal.sel(variable='pm25')
-    mean_pm_uncal.sel(variable='pm1')
+    pm_daily = pm.resample(time='1d').mean().mean(dim='station')
+    max_pm = pm_daily.idxmax(dim='time')
 
-    era5 = xr.open_dataset(DATA_DIR+'ERA5/era5_2021.nc').isel(expver=0)
-    # era5 = era5.resample(time='1d').mean()
-    era5 = era5.assign_coords(longitude = (era5.coords['longitude'].values+180)%360-180)
+    max_da = pm_daily.copy()
+    n_maxes = 3
+    higher_days = {}
+    for n_higher in range(1, n_maxes + 1):
+        max_pm = max_da.idxmax(dim='time')
+        higher_days[n_higher] = dict(zip(max_pm.coords['variable'].values, max_pm.values))
+        for pm_size, timestamp in zip(max_pm.coords['variable'].values, max_pm.values):
+            max_da.loc[pm_size, timestamp] = np.nan
+
+    higher_days
+
+    sec_max_pm = pm_daily.copy()
+    sec_max_pm.loc['pm10_cal', '2021-09-10T00:00:00.000000000'] = np.nan
+
+    era5 = xr.open_dataset(DATA_DIR + 'ERA5/era5_2021.nc').isel(expver=0)
+    era5 = era5.resample(time='1d').mean(skipna=True)
+    era5 = era5.assign_coords(longitude=(era5.coords['longitude'].values + 180) % 360 - 180)
     era5 = era5.sortby('longitude')
-    da_toplot = era5.sel(time='2021-08-03T23:00:00') \
-         .coarsen(latitude=3, longitude=3, boundary='trim').mean()\
-         .sortby('latitude')
 
-    ax = plt.subplot(projection=ccrs.Orthographic(-0.560294, 51.315831))
+    for n_high, pm_times in higher_days.items():
+        for pm_size in pm_times:
+            title = f'{n_high} max of {pm_size}'
+            plot_era5_wind(era5, pm_times[pm_size], title)
+
+
+    da_toplot = era5.sel(time='2021-08-03T00:00:00.000000000')\
+        .sortby('longitude').sel(longitude=slice(-2.4, 2)).sortby('latitude').sel(latitude=slice(50, 52))
+
+    ax = plt.subplot(projection=ccrs.Orthographic(-0.554106, 51.318604))
     p = da_toplot.plot.quiver(
         x='longitude', y='latitude',
         u='u10', v='v10',
-        subplot_kws=dict(projection=ccrs.Orthographic(-0.560294, 51.315831),
+        subplot_kws=dict(projection=ccrs.Orthographic(-0.554106, 51.318604),
                          facecolor="gray"),
         transform=ccrs.PlateCarree(),
     )
     p.axes.coastlines()
+    plt.title('abc')
     plt.show()
+
