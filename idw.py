9529a09ddcc2a660998c838021b0232619b497d0
Leonardo Kamigauti <leonardo.kamigauti@usp.br>
2022-01-06 21:55:59 +0000

    Plot loess

--- idw.py
+++ idw.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import xarray as xr
 import pandas as pd
 import numpy as np
+import pyinterp.backends.xarray
+import pyinterp.fill
 import winsound
 
 beep_duration = 1000  # milliseconds
@@ -33,9 +35,10 @@ if __name__ == '__main__':
     lat_res = 15
     lon_res = 30
 
-    # map_proj = ccrs.LambertConformal(central_longitude=central_longitude, central_latitude=central_latitude)
     request = cimgt.Stamen(style='terrain')
     map_proj = request.crs
+
+    # Plot 1 - Solo points
 
     p = pm_pd.to_xarray().pm1_cal
 
@@ -45,19 +48,55 @@ if __name__ == '__main__':
     lon_bins, lon_step = np.linspace(p.lon.min(), p.lon.max(), lon_res, retstep=True)
     lon_centres = np.linspace(p.lon.min() + (lon_step / 2), p.lon.max() + (lon_step / 2), lon_res - 1)
 
-
-    p = p.groupby_bins('lat', lat_bins, labels=lat_centres, include_lowest=True).mean()\
-        .groupby_bins('lon', lon_bins, labels=lon_centres, include_lowest=True).mean()\
+    p = p.groupby_bins('lat', lat_bins, labels=lat_centres, include_lowest=True).mean() \
+        .groupby_bins('lon', lon_bins, labels=lon_centres, include_lowest=True).mean() \
         .rename({'lat_bins': 'lat',
                  'lon_bins': 'lon'})
 
     p = p.plot(
         transform=ccrs.PlateCarree(),
         subplot_kws={'projection': map_proj},
+        alpha=0.75
     )
 
     p.axes.set_global()
     p.axes.coastlines()
-    p.axes.set_extent([-0.636, -0.456, 51.285, 51.380])
+    p.axes.set_extent([-0.636, -0.456, 51.285, 51.350])
     p.axes.add_image(request, 13)
+    p.axes.set_title("Interpol: None")
     plt.show()
+
+    # Plot 2 - Linear
+
+    p = pm_pd.to_xarray().pm1_cal
+    lat_bins, lat_step = np.linspace(p.lat.min(), p.lat.max(), lat_res, retstep=True)
+    lat_centres = np.linspace(p.lat.min() + (lat_step / 2), p.lat.max() - (lat_step / 2), lat_res - 1)
+
+    lon_bins, lon_step = np.linspace(p.lon.min(), p.lon.max(), lon_res, retstep=True)
+    lon_centres = np.linspace(p.lon.min() + (lon_step / 2), p.lon.max() + (lon_step / 2), lon_res - 1)
+
+    p = p.groupby_bins('lat', lat_bins, labels=lat_centres, include_lowest=True).mean() \
+        .groupby_bins('lon', lon_bins, labels=lon_centres, include_lowest=True).mean() \
+        .rename({'lat_bins': 'lat',
+                 'lon_bins': 'lon'})
+
+    grid = pyinterp.backends.xarray.Grid2D(p, geodetic=False)
+    filled = pyinterp.fill.loess(grid, nx=3, ny=3)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection=map_proj)
+    lons, lats = np.meshgrid(grid.x, grid.y, indexing='ij')
+    pcm = ax.pcolormesh(lons,
+                        lats,
+                        filled,
+                        cmap='viridis',
+                        shading='auto',
+                        transform=ccrs.PlateCarree(),
+                        alpha=0.75
+                        )
+    ax.set_title("Interpol: LOESS")
+    ax.set_extent([-0.636, -0.456, 51.285, 51.350])
+    ax.add_image(request, 13)
+    fig.colorbar(pcm, ax=[ax]).set_label('pm1')
+    fig.show()
+
