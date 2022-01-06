4869290c71678ab739309796d0a0b30f33c864e4
Leonardo Kamigauti <leonardo.kamigauti@usp.br>
2022-01-06 22:00:44 +0000

    Plot gauss-siedel

--- idw.py
+++ idw.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
     p.axes.set_title("Interpol: None")
     plt.show()
 
-    # Plot 2 - Linear
+    # Plot 2 - Loess
 
     p = pm_pd.to_xarray().pm1_cal
     lat_bins, lat_step = np.linspace(p.lat.min(), p.lat.max(), lat_res, retstep=True)
@@ -100,3 +100,36 @@ if __name__ == '__main__':
     fig.colorbar(pcm, ax=[ax]).set_label('pm1')
     fig.show()
 
+    # Plot 3 - Gauss-Seidel
+
+    p = pm_pd.to_xarray().pm1_cal
+    lat_bins, lat_step = np.linspace(p.lat.min(), p.lat.max(), lat_res, retstep=True)
+    lat_centres = np.linspace(p.lat.min() + (lat_step / 2), p.lat.max() - (lat_step / 2), lat_res - 1)
+
+    lon_bins, lon_step = np.linspace(p.lon.min(), p.lon.max(), lon_res, retstep=True)
+    lon_centres = np.linspace(p.lon.min() + (lon_step / 2), p.lon.max() + (lon_step / 2), lon_res - 1)
+
+    p = p.groupby_bins('lat', lat_bins, labels=lat_centres, include_lowest=True).mean() \
+        .groupby_bins('lon', lon_bins, labels=lon_centres, include_lowest=True).mean() \
+        .rename({'lat_bins': 'lat',
+                 'lon_bins': 'lon'})
+
+    grid = pyinterp.backends.xarray.Grid2D(p, geodetic=False)
+    has_converged, filled = pyinterp.fill.gauss_seidel(grid)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection=map_proj)
+    lons, lats = np.meshgrid(grid.x, grid.y, indexing='ij')
+    pcm = ax.pcolormesh(lons,
+                        lats,
+                        filled,
+                        cmap='viridis',
+                        shading='auto',
+                        transform=ccrs.PlateCarree(),
+                        alpha=0.75
+                        )
+    ax.set_title("Interpol: Gauss-Seidel")
+    ax.set_extent([-0.636, -0.456, 51.285, 51.350])
+    ax.add_image(request, 13)
+    fig.colorbar(pcm, ax=[ax]).set_label('pm1')
+    fig.show()
