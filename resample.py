2a3979e2cb10080dc2cd0ad4787d62ed620749f7
Leonardo Kamigauti <leonardo.kamigauti@usp.br>
2022-01-10 20:39:15 +0000

    Add grid pm

--- resample.py
+++ resample.py
@@ -10,9 +10,15 @@ OUTPUT_DIR = 'G:/My Drive/IC/Doutorado/Sandwich/Output/'
 LCS = 'WokingNetwork/'
 
 if __name__ == '__main__':
+    # resample pm hourly
     pm_path = OUTPUT_DIR + LCS + 'data_calibrated.nc'
     pm = xr.open_dataarray(pm_path)
     pm_hourly = tools.resample(pm, '1H')
     pm_hourly.to_netcdf(OUTPUT_DIR + LCS + 'data_hourly.nc')
 
-    
+    # grid pm
+    pm_lat_lon = tools.stations_to_lat_lon(OUTPUT_DIR + LCS + 'data_hourly.nc',
+                                           DATA_DIR + LCS + 'Woking Green Party deatils.csv',
+                                           save=True,
+                                           save_path=OUTPUT_DIR + LCS + 'data_lat_lon.nc')
+    pm_lat_lon
