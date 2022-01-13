fb069fd5cdaa28fffd701c9455adee300a49fcfc
Leonardo Kamigauti <leonardo.kamigauti@usp.br>
2022-01-13 20:45:56 +0000

    Init commit

--- invdisttree.py
+++ invdisttree.py
@@ -5,11 +5,13 @@
 from __future__ import division
 import numpy as np
 from scipy.spatial import cKDTree as KDTree
-    # http://docs.scipy.org/doc/scipy/reference/spatial.html
+
+# http://docs.scipy.org/doc/scipy/reference/spatial.html
 
 __date__ = "2010-11-09 Nov"  # weights, doc
 
-#...............................................................................
+
+# ...............................................................................
 class Invdisttree:
     """ inverse-distance-weighted interpolation using KDTree:
 invdisttree = Invdisttree( X, z )  -- data points, values
@@ -59,19 +61,20 @@ In contrast, the commonly-used Gaussian kernel exp( - (distance/h)**2 )
 is exceedingly sensitive to distance and to h.
 
     """
-# anykernel( dj / av dj ) is also scale-free
-# error analysis, |f(x) - idw(x)| ? todo: regular grid, nnear ndim+1, 2*ndim
 
-    def __init__( self, X, z, leafsize=10, stat=0 ):
+    # anykernel( dj / av dj ) is also scale-free
+    # error analysis, |f(x) - idw(x)| ? todo: regular grid, nnear ndim+1, 2*ndim
+
+    def __init__(self, X, z, leafsize=10, stat=0):
         assert len(X) == len(z), "len(X) %d != len(z) %d" % (len(X), len(z))
-        self.tree = KDTree( X, leafsize=leafsize )  # build the tree
+        self.tree = KDTree(X, leafsize=leafsize, copy_data=True)  # build the tree
         self.z = z
         self.stat = stat
         self.wn = 0
         self.wsum = None;
 
-    def __call__( self, q, nnear=6, eps=0, p=1, weights=None ):
-            # nnear nearest neighbours of each query point --
+    def __call__(self, q, nnear=6, eps=0, p=1, weights=None):
+        # nnear nearest neighbours of each query point --
         q = np.asarray(q)
         qdim = q.ndim
         if qdim == 1:
@@ -79,28 +82,29 @@ is exceedingly sensitive to distance and to h.
         if self.wsum is None:
             self.wsum = np.zeros(nnear)
 
-        self.distances, self.ix = self.tree.query( q, k=nnear, eps=eps )
-        interpol = np.zeros( (len(self.distances),) + np.shape(self.z[0]) )
+        self.distances, self.ix = self.tree.query(q, k=nnear, eps=eps)
+        interpol = np.zeros((len(self.distances),) + np.shape(self.z[0]))
         jinterpol = 0
-        for dist, ix in zip( self.distances, self.ix ):
+        for dist, ix in zip(self.distances, self.ix):
             if nnear == 1:
                 wz = self.z[ix]
             elif dist[0] < 1e-10:
                 wz = self.z[ix[0]]
             else:  # weight z s by 1/dist --
-                w = 1 / dist**p
+                w = 1 / dist ** p
                 if weights is not None:
                     w *= weights[ix]  # >= 0
                 w /= np.sum(w)
-                wz = np.dot( w, self.z[ix] )
+                wz = np.dot(w, self.z[ix])
                 if self.stat:
                     self.wn += 1
                     self.wsum += w
             interpol[jinterpol] = wz
             jinterpol += 1
-        return interpol if qdim > 1  else interpol[0]
+        return interpol if qdim > 1 else interpol[0]
 
-#...............................................................................
+
+# ...............................................................................
 if __name__ == "__main__":
     import sys
 
@@ -114,31 +118,33 @@ if __name__ == "__main__":
     cycle = .25
     seed = 1
 
-    exec "\n".join( sys.argv[1:] )  # python this.py N= ...
-    np.random.seed(seed )
-    np.set_printoptions( 3, threshold=100, suppress=True )  # .3f
+    # exec "\n".join( sys.argv[1:] )  # python this.py N= ...
+    np.random.seed(seed)
+    np.set_printoptions(3, threshold=100, suppress=True)  # .3f
 
-    print "\nInvdisttree:  N %d  Ndim %d  Nask %d  Nnear %d  leafsize %d  eps %.2g  p %.2g" % (
-        N, Ndim, Nask, Nnear, leafsize, eps, p)
+
+    # print "/nInvdisttree:  N %d  Ndim %d  Nask %d  Nnear %d  leafsize %d  eps %.2g  p %.2g" % (
+    #     N, Ndim, Nask, Nnear, leafsize, eps, p)
 
     def terrain(x):
         """ ~ rolling hills """
-        return np.sin( (2*np.pi / cycle) * np.mean( x, axis=-1 ))
+        return np.sin((2 * np.pi / cycle) * np.mean(x, axis=-1))
 
-    known = np.random.uniform( size=(N,Ndim) ) ** .5  # 1/(p+1): density x^p
-    z = terrain( known )
-    ask = np.random.uniform( size=(Nask,Ndim) )
 
-#...............................................................................
-    invdisttree = Invdisttree( known, z, leafsize=leafsize, stat=1 )
-    interpol = invdisttree( ask, nnear=Nnear, eps=eps, p=p )
+    known = np.random.uniform(size=(N, Ndim)) ** .5  # 1/(p+1): density x^p
+    z = terrain(known)
+    ask = np.random.uniform(size=(Nask, Ndim))
 
-    print "average distances to nearest points: %s" % \
-        np.mean( invdisttree.distances, axis=0 )
-    print "average weights: %s" % (invdisttree.wsum / invdisttree.wn)
-        # see Wikipedia Zipf's law
-    err = np.abs( terrain(ask) - interpol )
-    print "average |terrain() - interpolated|: %.2g" % np.mean(err)
+    # ...............................................................................
+    invdisttree = Invdisttree(known, z, leafsize=leafsize, stat=1)
+    interpol = invdisttree(ask, nnear=Nnear, eps=eps, p=p)
+
+    # print "average distances to nearest points: %s" % \
+    np.mean(invdisttree.distances, axis=0)
+    # print "average weights: %s" % (invdisttree.wsum / invdisttree.wn)
+    #     # see Wikipedia Zipf's law
+    # err = np.abs( terrain(ask) - interpol )
+    # print "average |terrain() - interpolated|: %.2g" % np.mean(err)
 
     # print "interpolate a single point: %.2g" % \
     #     invdisttree( known[0], nnear=Nnear, eps=eps )
