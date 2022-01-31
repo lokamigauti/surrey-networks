019d4688d39c6458aeefcc2b161a813ed6c7cffe
Leonardo Kamigauti <leonardo.kamigauti@usp.br>
2022-01-31 18:39:35 +0000

    Set roads to produce one raster per function

--- roads.py
+++ roads.py
@@ -31,35 +31,47 @@ if __name__ == '__main__':
 
     roads.to_file(DATA_DIR + ROADS + 'roads.shp')
 
-    categorical_enums = {'function': roads['function'].unique()}
+    # Trying to condense the informations in a single raster, without success.
+    #
+    # categorical_enums = {'function': roads['function'].unique()}
+    #
+    # out_grid = make_geocube(
+    #     vector_data=roads,
+    #     resolution=RESOLUTION,
+    #     categorical_enums=categorical_enums
+    # )
+    #
+    # out_grid.function.plot()
+    # plt.show()
+    #
+    # function_string = out_grid['function_categories'][out_grid['function'].astype(int)] \
+    #     .drop('function_categories')
+    #
+    # out_grid['function'] = function_string
+    # pdf = out_grid.drop(['spatial_ref', 'function_categories']).to_dataframe()
+    # cat_dtype = pd.api.types.CategoricalDtype(out_grid.function_categories.values)
+    # pdf['function'] = pdf['function'].astype(cat_dtype)
+    #
+    # training_df = pd.get_dummies(pdf, columns=['function'])
+    # training_df.head()
 
-    out_grid = make_geocube(
-        vector_data=roads,
+
+    for road_func in roads['function'].unique():
+        roads_subset = roads.loc[roads['function'] == road_func]
+        out_grid = make_geocube(
+            vector_data=roads_subset,
+            resolution=RESOLUTION,
+        )
+
+        file_name = road_func + '_' + str(RESOLUTION[0]) + 'x' + str(RESOLUTION[1]) + '.tif'
+        out_grid.rio.to_raster(DATA_DIR + ROADS + FUNCTIONS + file_name.replace(' ', '_'))
+
+    roads_minor = roads.loc[roads['function'] == 'Minor Road']
+    roads_minor = roads_minor.filter(['function', 'geometry'])
+
+    minor_grid = make_geocube(
+        vector_data=roads_minor,
         resolution=RESOLUTION,
-        categorical_enums=categorical_enums
     )
 
-    out_grid.function.plot()
-    plt.show()
-
-    function_string = out_grid['function_categories'][out_grid['function'].astype(int)] \
-        .drop('function_categories')
-
-    out_grid['function'] = function_string
-    pdf = out_grid.drop(['spatial_ref', 'function_categories']).to_dataframe()
-    cat_dtype = pd.api.types.CategoricalDtype(out_grid.function_categories.values)
-    pdf['function'] = pdf['function'].astype(cat_dtype)
-
-    training_df = pd.get_dummies(pdf, columns=['function'])
-    training_df.head()
-
-
-    # for road_func in roads['function'].unique():
-    #     roads_subset = roads.loc[roads['function'] == road_func]
-    #     out_grid = make_geocube(
-    #         vector_data=roads_subset,
-    #         resolution=RESOLUTION,
-    #     )
-    #
-    #     file_name = road_func + '_' + str(RESOLUTION[0]) + 'x' + str(RESOLUTION[1]) + '.tif'
-    #     out_grid.rio.to_raster(DATA_DIR + ROADS + FUNCTIONS + file_name.replace(' ', '_'))
+    
