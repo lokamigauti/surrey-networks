0669e2b3befb77c78cc5fbb735a13a6a3ac6817a
Leonardo Kamigauti <leonardo.kamigauti@usp.br>
2022-02-01 15:23:58 +0000

    Add 2022

--- import_ERA5.py
+++ import_ERA5.py
@@ -34,12 +34,13 @@ def retrieve_era5(c, year, months, days, hours, params, area = [0, 0, 0, 0]):
 
 
 if __name__ == '__main__':
-    c = cdsapi.Client()
+    c = cdsapi.Client(timeout=600)
 
     params = [
         '10m_u_component_of_wind', '10m_v_component_of_wind', '2m_dewpoint_temperature', '2m_temperature',
         'boundary_layer_dissipation', 'boundary_layer_height', 'forecast_surface_roughness', 'low_cloud_cover',
-        'soil_type', 'surface_pressure', 'total_cloud_cover', 'total_precipitation',
+        'soil_type', 'total_cloud_cover', 'total_precipitation', 'surface_net_solar_radiation',
+        'mean_total_precipitation_rate'
     ]
 
     UK_data_window = [
@@ -49,7 +50,7 @@ if __name__ == '__main__':
     hours = [f'{x:02d}' + ':00' for x in np.arange(0, 24)]
     months = [f'{x:02d}' for x in np.arange(1, 13)]
     days = [f'{x:02d}' for x in np.arange(1, 32)]
-    years = [2021]
+    years = [2021, 2022]
     for year in years:
         retrieve_era5(c, year, months, days, hours, params, UK_data_window)
 
